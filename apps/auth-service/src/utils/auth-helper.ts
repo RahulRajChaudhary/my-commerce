443ff23f488d5dae843df1ae6380f7c7b2bafb8e
Rahul Raj <rrajchaudhary.hajipur@gmail.com>
2025-06-18 19:10:26 +0530

    otp and mailing template set

--- apps/auth-service/src/utils/auth-helper.ts
+++ apps/auth-service/src/utils/auth-helper.ts
@@ -1,6 +1,7 @@
 
 import crypto from 'crypto';
 import { ValidationError } from '../../../../packages/error-handler/index';
+import redis from '../../../../packages/libs/redis/index';
 
 
 const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
@@ -29,8 +30,13 @@ export const checkOtpRestrictions = async (email: string, next: NewableFunction)
 }
 
 
-export const sendOtp = async (name: string, email: string ,template: string) => {
-  const otp = crypto.randomInt(1000, 9999).toString;
-  
-  // seting the otp in redis database with expire
-}
+export const sendOtp = async (
+  name: string,
+  email: string,
+  template: string
+) => {
+  const otp = crypto.randomInt(1000, 9999).toString();
+  await sendEmail(email, "Verify Your Email", template, { name, otp });
+  await redis.set(`otp:${email}`, otp, "EX", 300);
+  await redis.set(`otp_cooldown:${email}`, "true", "EX", 60);
+};
